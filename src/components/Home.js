da3637599091ea5c981f73e11363b8dbc011ef0c
Jonathan Williams <jonathanwilliams96@hotmail.com>
2019-04-26 08:00:33 -0400

    navBar color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 const Home = () => <h1 style={{textAlign:"center", display:"block", fontSize: "100px", color:"lightblue"}}>Bookshelf Time!!!</h1>
+
+
 export default Home
